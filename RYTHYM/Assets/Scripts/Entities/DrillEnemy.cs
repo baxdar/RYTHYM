bf9742086577695af9faf1a4a0e7a67bf775fb98
Kyle Espinoza <kylepespinoza@gmail.com>
2017-06-18 21:42:30 -0600

    Small, Medium, Large Metal Platforms Drill Enemy    -Sprites    -Animations/State Machine

---
+++ RYTHYM/Assets/Scripts/Entities/DrillEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillEnemy : MonoBehaviour {
+
+    private Animator anim;
+    public bool detectingPlayer;
+
+	void Awake () {
+        anim = GetComponent<Animator>();
+	}
+    void Start()
+    {
+        anim.SetBool("idle", true);
+    }
+    private void Update()
+    {
+        detectingPlayer = Physics2D.OverlapBox(transform.position, new Vector2(4f, 1f), 0f);
+    }
+    IEnumerator DrillDown()
+    {
+        yield return new WaitForSeconds(3f);
+        anim.SetBool("idle", false);
+        anim.SetBool("drillingDown", true);
+    }
+}
